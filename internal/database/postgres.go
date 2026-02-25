09d5fdf70241cf5d52de744de49bb27f0c4457dc
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-02-25 23:55:40 +0100

    feat: add postgres database and run migrations

---
+++ internal/database/postgres.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/viper"
+	"github.com/wimwenigerkind/odoopack-registry/internal/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s",
+		viper.GetString("db.host"),
+		viper.GetString("db.user"),
+		viper.GetString("db.password"),
+		viper.GetString("db.name"),
+		viper.GetString("db.port"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
+func Migrate(db *gorm.DB) error {
+	if db == nil {
+		return fmt.Errorf("database connection not established")
+	}
+
+	err := db.AutoMigrate(
+		&models.Addon{},
+		&models.AddonVersion{},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	log.Println("database migrations completed successfully")
+	return nil
+}
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"github.com/wimwenigerkind/odoopack-registry/internal/database"
 )
 
 func main() {
@@ -14,15 +16,34 @@ func main() {
 
 	viper.SetDefault("server_address", "0.0.0.0:6969")
 
+	viper.SetDefault("db.host", "localhost")
+	viper.SetDefault("db.port", "5432")
+	viper.SetDefault("db.user", "postgres")
+	viper.SetDefault("db.password", "postgres")
+	viper.SetDefault("db.name", "odoopack-registry")
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println("no config file found, using defaults")
 	}
 
+	db, err := database.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	err = database.Migrate(db)
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 
-	err := r.Run(viper.GetString("server_address"))
+	fmt.Printf("starting on: http://%v\n", viper.GetString("server_address"))
+
+	err = r.Run(viper.GetString("server_address"))
 	if err != nil {
 		panic(err)
 	}
