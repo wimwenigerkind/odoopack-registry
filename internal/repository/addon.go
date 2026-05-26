e1d9467869c5e27bd5ca319e0b140978461d5813
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-05-26 13:34:51 +0200

    feat: add addon and version repositories

--- internal/repository/addon.go
+++ internal/repository/addon.go
@@ -1,1 +1,57 @@
 package repository
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/wimwenigerkind/odoopack-registry/internal/models"
+	"gorm.io/gorm"
+)
+
+var ErrNotFound = errors.New("repository: record not found")
+
+type AddonRepository struct {
+	db *gorm.DB
+}
+
+func NewAddonRepository(db *gorm.DB) *AddonRepository {
+	return &AddonRepository{db: db}
+}
+
+func (r *AddonRepository) Create(addon *models.Addon) error {
+	return r.db.Create(addon).Error
+}
+
+func (r *AddonRepository) GetByID(id uuid.UUID) (*models.Addon, error) {
+	var addon models.Addon
+	err := r.db.Preload("Versions").Where("id = ?", id).First(&addon).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, ErrNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &addon, nil
+}
+
+func (r *AddonRepository) GetByName(name string) (*models.Addon, error) {
+	var addon models.Addon
+	err := r.db.Preload("Versions").Where("name = ?", name).First(&addon).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, ErrNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &addon, nil
+}
+
+func (r *AddonRepository) List(nameFilter string) ([]models.Addon, error) {
+	var addons []models.Addon
+	q := r.db.Preload("Versions")
+	if nameFilter != "" {
+		q = q.Where("name = ?", nameFilter)
+	}
+	err := q.Find(&addons).Error
+	return addons, err
+}
---
+++ internal/repository/version.go
@@ -0,0 +1,74 @@
+package repository
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/wimwenigerkind/odoopack-registry/internal/models"
+	"gorm.io/gorm"
+)
+
+type AddonVersionRepository struct {
+	db *gorm.DB
+}
+
+func NewAddonVersionRepository(db *gorm.DB) *AddonVersionRepository {
+	return &AddonVersionRepository{db: db}
+}
+
+func (r *AddonVersionRepository) Upsert(version *models.AddonVersion) error {
+	var existing models.AddonVersion
+	err := r.db.Where("addon_id = ? AND version = ?", version.AddonID, version.Version).First(&existing).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return r.db.Create(version).Error
+	}
+	if err != nil {
+		return err
+	}
+	existing.RefType = version.RefType
+	existing.RefValue = version.RefValue
+	existing.Status = version.Status
+	if version.StorageKey != "" {
+		existing.StorageKey = version.StorageKey
+	}
+	if version.ContentHash != "" {
+		existing.ContentHash = version.ContentHash
+	}
+	if version.SizeBytes != 0 {
+		existing.SizeBytes = version.SizeBytes
+	}
+	*version = existing
+	return r.db.Save(version).Error
+}
+
+func (r *AddonVersionRepository) Get(addonID uuid.UUID, version string) (*models.AddonVersion, error) {
+	var av models.AddonVersion
+	err := r.db.Where("addon_id = ? AND version = ?", addonID, version).First(&av).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, ErrNotFound
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &av, nil
+}
+
+func (r *AddonVersionRepository) UpdateStatus(id uuid.UUID, status models.VersionStatus, buildErr string) error {
+	return r.db.Model(&models.AddonVersion{}).Where("id = ?", id).Updates(map[string]any{
+		"status":      status,
+		"build_error": buildErr,
+	}).Error
+}
+
+func (r *AddonVersionRepository) SetReady(id uuid.UUID, storageKey, contentHash string, sizeBytes int64) error {
+	now := time.Now()
+	return r.db.Model(&models.AddonVersion{}).Where("id = ?", id).Updates(map[string]any{
+		"status":       models.StatusReady,
+		"storage_key":  storageKey,
+		"content_hash": contentHash,
+		"size_bytes":   sizeBytes,
+		"built_at":     now,
+		"build_error":  "",
+	}).Error
+}
