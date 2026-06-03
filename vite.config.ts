11ecf3a9b525d17f03cb44a1a4f42825c30b80b5
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2026-06-03 21:13:19 +0200

    feat: configure path aliases and server settings for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // production serves static build.
+    allowedHosts: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
